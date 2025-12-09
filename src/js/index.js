6403e75f301ab4a07e25f66d08e3d95e0e80c24b
PedroHss <161982260+Pdoh51@users.noreply.github.com>
2025-12-09 18:45:22 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-const SAVE_ATIVO = true;
+const SAVE_ATIVO = false;
 
 function obterIdUsuario() {
     let id = localStorage.getItem("idUsuario");
@@ -1518,5 +1518,6 @@ window.addEventListener("load", () => {
     caixaDialogo.style.display = "none";
     introducao.style.display = "none";
 });
+
 
 
